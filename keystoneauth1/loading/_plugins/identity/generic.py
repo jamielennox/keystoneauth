d8a9d716b0d4e492dadc27fc102817c2ba0e5813
Jamie Lennox <jamielennox@redhat.com>
2015-09-15 13:38:09 +1000

    Support DNS lookup for generic plugins
    Change-Id: Ia1ee07710d955d949909313ed51643f34097dbf7

--- keystoneauth1/loading/_plugins/identity/generic.py
+++ keystoneauth1/loading/_plugins/identity/generic.py
@@ -10,12 +10,24 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import logging
+
+try:
+    import dns.resolver as dns_resolver
+    import dns.exception as dns_exception
+except ImportError:
+    dns_exception = dns_resolver = None
+
 from keystoneauth1 import identity
 from keystoneauth1 import loading
 from keystoneauth1.loading._plugins.identity import base
 
+LOG = logging.getLogger(__name__)
+
 
 class GenericBaseLoader(base.BaseIdentityLoader):
+
+    DNS_NAME = '_openstack_keystone'
 
     def get_options(self):
         options = super(GenericBaseLoader, self).get_options()
@@ -35,6 +47,40 @@ class GenericBaseLoader(base.BaseIdentityLoader):
         ])
 
         return options
+
+    def _get_dns_name(self):
+        if not (dns_resolver and dns_exception):
+            LOG.debug("Skipping DNS lookup as dnspython not installed")
+            return None
+
+        try:
+            response = dns_resolver.query(self.DNS_NAME,
+                                          'TXT',
+                                          raise_on_no_answer=True)
+        except (dns_resolver.NoAnswer, dns_resolver.NXDOMAIN):
+            LOG.debug("No authentication URL discovered from DNS")
+        except dns_resolver.Timeout:
+            LOG.warn("Timed out trying to discover URL from DNS")
+        except dns_exception.DNSException as e:
+            LOG.warn("Unexpected DNS error: %s", e)
+        else:
+            urls = [s for answer in response for s in answer.strings]
+
+            if len(urls) > 1:
+                LOG.warn("Found multiple options for URL from DNS. "
+                         "Ignoring all of them rather than picking one.")
+            else:
+                LOG.debug("Using auth url from dns: %s", urls[0])
+                return urls[0]
+
+    def load_from_options(self, **kwargs):
+        if not kwargs.get('auth_url'):
+            auth_url = self._get_dns_name()
+
+            if auth_url:
+                kwargs['auth_url'] = auth_url
+
+        return super(GenericBaseLoader, self).load_from_options(**kwargs)
 
 
 class Token(GenericBaseLoader):
